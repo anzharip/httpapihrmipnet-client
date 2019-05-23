c43a53dff644ceb38618c0dde78d754660909c92
anzhari.p@gmail.com <anzhari.p@gmail.com>
2019-05-23 13:51:35 +0700

    implemented personaldetail.html

--- app.js
+++ app.js
@@ -58,7 +58,11 @@ var app_personal_detail_attachment = new Vue({
     data: {
         attachment: [],
         selected_attachment: {},
-        upload_attachment: {},
+        upload_attachment: {
+            select_file: "",
+            file_name: "",
+            comment: ""
+        },
         message: ""
     },
     methods: {
@@ -71,10 +75,12 @@ var app_personal_detail_attachment = new Vue({
             model_attachment.delete(data).then(function (response) {
                 console.log(response);
                 self.message = response.data.message;
+                $('#attachmentPersonalDetailModal').modal('show');
                 self.getAttachment("all");
             }).catch(function (error) {
                 console.log(error.response);
                 self.message = error.response.data.message;
+                $('#attachmentPersonalDetailModal').modal('show');
             });
             console.log(self.message);
         },
@@ -86,41 +92,78 @@ var app_personal_detail_attachment = new Vue({
             var self = this;
             model_attachment.get(data).then(function (response) {
                 self.attachment = response.data;
-                self.message = response.data.message;
+                // Don't enable this as this will cause the modal to always show file succesfully retrieved
+                // self.message = response.data[0].message;
             }).catch(function (error) {
                 console.log(error.response);
                 self.message = error.response.data.message;
+                $('#attachmentPersonalDetailModal').modal('show');
             });
         },
         selectAttachment: function (file) {
-            var self = this; 
-            self.selected_attachment = file; 
+            var self = this;
+            self.selected_attachment = file;
             self.showEditAttachment();
         },
         cancelEditAttachment: function () {
-            var self = this; 
-            self.selected_attachment = {}; 
+            var self = this;
+            self.selected_attachment = {};
             self.hideEditAttachment();
         },
         submitEditAttachment: function () {
             var model_attachment = new ModelAttachment(config);
-            var self = this; 
-            var data = self.selected_attachment; 
+            var self = this;
+            var data = self.selected_attachment;
             model_attachment.put(data).then(function (response) {
                 console.log(response);
                 self.message = response.data.message;
+                $('#attachmentPersonalDetailModal').modal('show');
                 self.getAttachment("all");
                 self.cancelEditAttachment();
             }).catch(function (error) {
                 console.log(error.response);
                 self.message = error.response.data.message;
-            }); 
+                $('#attachmentPersonalDetailModal').modal('show');
+            });
         },
         hideEditAttachment: function () {
             $('.editAttachment').prop("hidden", true);
         },
         showEditAttachment: function () {
             $('.editAttachment').prop("hidden", false);
+        },
+        convertUploadAttachment: function (event) {
+            var self = this;
+            console.log(event.target.files[0]);
+            self.upload_attachment.file_name = event.target.files[0].name;
+            var reader = new FileReader();
+            reader.readAsDataURL(event.target.files[0]);
+            reader.onload = function () {
+                self.upload_attachment.select_file = reader.result.split(",")[1];
+                console.log(reader.result.split(",")[1]);
+            };
+        },
+        cancelUploadAttachment: function () {
+            var self = this;
+            self.upload_attachment = {
+                select_file: "",
+                file_name: "",
+                comment: ""
+            }
+        },
+        submitUploadAttachment: function () {
+            var self = this;
+            var model_attachment = new ModelAttachment(config);
+            var data = self.upload_attachment;
+            model_attachment.post(data).then(function (response) {
+                console.log(response);
+                self.message = response.data.message;
+                $('#attachmentPersonalDetailModal').modal('show');
+                self.getAttachment("all");
+            }).catch(function (error) {
+                console.log(error.response);
+                self.message = error.response.data.message;
+            });
         }
     }
 });
